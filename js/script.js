51b69cec4ecb7063a6c72bdd7728b7e8a4568b59
denis <ovicartel@gmail.com>
2021-07-04 23:56:50 +0300

    // Управление прокруткой страницы scrollIntoView()

--- js/script.js
+++ js/script.js
@@ -85,6 +85,36 @@ function setScrollToOptions() {
 		// Проблема в том что эти опции не работают в Safari и широкого приминения у них нет в таком виде.
 	});
 }
+// ------------------------------
+// Управление прокруткой страницы
+// Вызов elem.scrollIntoView(top) прокручивает страницу,
+// чтобы elem оказался вверху. У него есть 1 аргумент:
+
+/*
+- если top = true(по умолчанию), то страница будет прокручена, чтобы elem 
+появился в верхней части окна.
+Верхний край элемента совмещён с верхней частью окна.
+- если top = false, то страница будет прокручена, что бы elem появился внизу. Нижний край элемента будет совмещён с нижним краем окна.
+*/
+
+function setScrollIntoView(top) {
+	const lessonSelected = document.querySelector('.lesson__selected');
+	lessonSelected.scrollIntoView(top);
+}
+
+function setScrollIntoViewOptions(top) {
+	const lessonSelected = document.querySelector('.lesson__selected');
+	lessonSelected.scrollIntoView({
+		// "start", 'center', 'end' или 'nearest'. По умолчанию 'center'.
+		block: 'center',
+		// "start", 'center', 'end' или 'nearest'. По умолчанию 'nearest'.
+		inline: 'nearest',
+		// 'auto' или 'smooth'. По умолчанию 'smooth'.
+		behavior: 'smooth'
+	});
+}
+// опции не работают в Safari
+
 //ДОМАШКА
 /*
 1. Изучить теорию
