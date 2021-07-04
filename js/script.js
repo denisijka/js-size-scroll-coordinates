2ef13733e4c8f3bd82f442a0b7ef27f6fe428eb5
denis <ovicartel@gmail.com>
2021-07-04 14:11:21 +0300

    // 1. Размеры окна браузера  clientWidth и clientHeight

--- js/script.js
+++ js/script.js
@@ -3,6 +3,24 @@
 // clientWidth и clientHeight
 
 // Доступная ширина и высота окна. 
+const mainElement = document.documentElement;
+const mainElementWidth = mainElement.clientWidth;
+const mainElementHeight = mainElement.clientHeight;
+
+console.log(mainElementWidth);
+console.log(mainElementHeight);
+
+// ------------------------------
+
+// Ширина и высота окна вместе с полосами прокрутки
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+
+console.log(windowWidth);
+console.log(windowHeight);
+
+
+
 //ДОМАШКА
 /*
 1. Изучить теорию
